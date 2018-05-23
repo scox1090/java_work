a79dedd1a60b4492fb3aa714084d299a293dea3a
KOITT <KOITT@KOITT-PC>
2018-05-23 21:28:23 +0900

    34쪽 예제까지 완료

---
+++ src/ch01/VarEx02.java
@@ -0,0 +1,19 @@
+package ch01;
+
+public class VarEx02 {
+		public static void main(String[] args) {
+			int x = 10, y = 20; // 변수 x, y를 선언하고, 각각 10과 20으로 초기화
+			int tmp = 0; // 변수 tmp를 선언하고, 0으로 초기화
+			
+			System.out.println("x:"+ x + " y:" + y); //x와 y값 출력
+			
+			tmp = x; // x의 값을 tmp 변수에 저장
+			x = y; // x변수에 y값을 저장
+			y = tmp; // y변수에 tmp 값을 저장
+			
+			System.out.println("x:"+ x + " y:" + y); //x와 y값 출력
+			
+			/* 이 코드의 목적은 : x와 y값을 바뀌치기 하는 방법이다.*/
+		}
+	}
+
